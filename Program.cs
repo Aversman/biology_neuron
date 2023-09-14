720b76086aa0e0af2dc78349d434f503e511419d
Aversman <aversmanu@mail.ru>
2023-09-14 17:35:15 +0500

    last update

--- Program.cs
+++ Program.cs
@@ -7,5 +7,20 @@ class Program {
     if (neurons == null) {
       throw new NeuronException("input data was not written");
     }
+    // Количество тактов
+    int n = 3;
+    // Входные данные
+    int[,] data = {
+      {0, 1, 1, 0},
+      {0, 0, 1, 1},
+      {1, 0, 1, 0}
+    };
+
+    Network net = new Network(n, data, neurons);
+    net.RunProcess();
+
+    for (int i = 0; i < neurons.Length; i++) {
+      Console.WriteLine("Neuron #" + i + ": " + neurons[i].Output);
+    }
   }
 }
--- src/Network.cs
+++ src/Network.cs
@@ -1,37 +1,33 @@
-class GraphException : Exception {
-  public GraphException(string str) : base(str) {
+class NetworkException : Exception {
+  public NetworkException(string str) : base(str) {
     Console.WriteLine(str);
   }
 }
 
-class Graph {
+class Network {
+  // Массив нейронов
   Neuron[]? neurons;
   // Входные слова за все итерации
   int[,] inputs;
   // Количество итераций
   int n;
-  public int Result;
-  public Graph(int n, int[,] inputs, Neuron[] neurons) {
+  public Network(int n, int[,] inputs, Neuron[] neurons) {
     this.n = n;
     this.inputs = inputs;
     this.neurons = neurons;
-    Result = 0;
     if (inputs == null) {
-      throw new GraphException("Wrong input data");
-    }
-    if (neurons == null) {
-      throw new GraphException("Wrong input data");
-    }
-    if (inputs.Length != n) {
-      throw new GraphException("Wrong input data");
+      throw new NetworkException("inputs data must not be null");
     }
   }
+  // Синапсы сортируются. Сначала рассматриваются не висячие синапсы
+  // Далее из входного слова по порядку заполняются висячие синапсы
+  // Тем самым образуем входное слово для нейрона и выполняем его процесс
   void ExecProcess(int curTime) {
-    if (neurons == null) throw new GraphException("Wrong input data");
+    if (neurons == null) throw new NetworkException("neurons data must not be null");
     int cursor = 0;
     foreach (var neuron in neurons) {
-      if (neuron.Synapses == null) throw new GraphException("Wrong input data");
-      if (neuron.Connectors == null) throw new GraphException("Wrong input data");
+      if (neuron.Synapses == null) throw new NetworkException("Neuron.Synapses must not be null");
+      if (neuron.Connectors == null) throw new NetworkException("Neuron.Connectors must not be null");
       int[] inputWord = new int[neuron.Synapses.Length];
       int i;
       for (i = 0; i < neuron.Connectors.Length; i++) {
